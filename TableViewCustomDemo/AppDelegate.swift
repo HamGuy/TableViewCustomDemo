995d2df58cae6eb26c1c59684006c04d67ddf644
WangRui <wangrui@dxyer.com>
2017-01-13 10:05:03 +0800

    Initial Commit

---
+++ TableViewCustomDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableViewCustomDemo
+//
+//  Created by HamGuy on 13/01/2017.
+//  Copyright © 2017 HamGuy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
